ea1411e1408a7c2a1559d86296e4bbac9720c9e3
Unknown <buzamahmooza@gmail.com>
2018-09-25 20:00:45 +0300

    Ignore Scraps/ directory

--- Scraps/gfycat url to gif url.js
+++
@@ -1,7 +0,0 @@
-
-
-// https://gfycat.com/gifs/detail/FearfulPlaintiveGrison
-// https://thumbs.gfycat.com/FearfulPlaintiveGrison-size_restricted.gif
-function gfycatPage2GifUrl(gfycatPageUrl) {
-    return "https://thumbs.gfycat.com/" + gfycatPageUrl.split('/').pop() + "-size_restricted.gif";
-}
