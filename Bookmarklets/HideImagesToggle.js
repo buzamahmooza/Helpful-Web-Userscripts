5b174ac8da742fb7f0bbaf46beb7df01c73c90f9
Faris Hijazi <theefaris@gmail.com>
2019-04-08 12:01:05 +0300

    Fixed hiding images not working for background images

--- Bookmarklets/HideImagesToggle.js
+++ Bookmarklets/HideImagesToggle.js
@@ -1,9 +1,18 @@
 
 JavaScript:(function hideImagesToggle() {
-    const w = 0.7; /*weight between [0,1], 1 will put all the weight on the client dimensions*/
-    const isBigImg = img => ((1-w)*Math.min(img.naturalHeight, img.naturalWidth) + w*Math.min(img.clientWidth, img.clientHeight) >= 35);
-    const imgs = Array.from(document.images).filter(isBigImg);
-    const bgimgs = getElementsWithCssProperty('background-image').filter(isBigImg);
+    const weight = 0.7; /*weight between [0,1], 1 will put all the weight on the client dimensions*/
+    const isBigImg = (w, h) => ((1-weight)*Math.min(w, h) + weight*Math.min(w, h) >= 35);
+    const imgs = Array.from(document.images).filter(img=>isBigImg(img.naturalWidth, img.naturalHeight));
+    const bgimgs = getElementsWithCssProperty('background-image').filter(bgimg=>{
+        var image = new Image();
+        image.src = bgimg.style /*create an image using the src*/
+              .backgroundImage
+               .replace(/url\((['"])?(.*?)\1\)/gi, '$2')
+                .split(',')[0];
+        console.log(image.width, 'x', image.height);
+        return isBigImg(image.width, image.height);
+    });
+
     const combined = imgs.concat(bgimgs);
     
     console.log(
